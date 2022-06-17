74259905c0703a42965be65ab58bb7eb8713bcef
mailto:haroon@staytuned.digital <mailto:haroon@staytuned.digital>
2022-06-17 19:53:26 +0500

    Added Api Methods

--- api/builderSettings/index.js
+++ api/builderSettings/index.js
@@ -5,3 +5,15 @@ import { BASE_URL } from '../../constants/constants';
 export const createBuilderSettings = async (object) => {
     return await resolve(axios.post(`${BASE_URL}create-builder-settings/`,object).then(res => res.data));
 }
+
+export const editBuilderSettings = async (id) => {
+    return await resolve(axios.post(`${BASE_URL}create-builder-settings/${id}/`,object).then(res => res.data));
+}
+
+export const getBuilderSettings = async () => {
+    return await resolve(axios.get(`${BASE_URL}create-builder-settings/`).then(res => res.data));
+}
+
+export const deleteBuilderSettings = async (id) => {
+    return await resolve(axios.delete(`${BASE_URL}create-builder-settings/${id}/`).then(res => res.data));
+}
