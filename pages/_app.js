a362bdec9baf004ec3454f9c18eef4139e89945c
mailto:haroon@staytuned.digital <mailto:haroon@staytuned.digital>
2022-07-13 13:16:57 -0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
 import '../styles/globals.css'
-import { LoaderProvider, useLoading,ThreeDots } from '@agney/react-loading';
+import { LoaderProvider, useLoading, ThreeDots } from '@agney/react-loading';
 import '../styles/BuildersForm/buildersSettingsForm.css'
 import '../styles/Modals/infoModal.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'
 import LoaderScreen from '../components/loader'
-import { Provider } from 'react-redux';
 import store from '../redux/store/store';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
     <LoaderProvider indicator={<LoaderScreen />}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </LoaderProvider>
-    </Provider>
   )
 }
 
