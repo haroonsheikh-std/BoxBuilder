57d6c843c13e08627f669cdbaf9aa6a176b4d5f2
mailto:haroon@staytuned.digital <mailto:haroon@staytuned.digital>
2022-07-15 07:52:43 -0500

    merge

--- components/builderForms/discountsForm/discountModal/discountModal.jsx
+++ components/builderForms/discountsForm/discountModal/discountModal.jsx
@@ -3,16 +3,10 @@ import { Form, Button, Row, Col, Spinner, Dropdown, Modal } from "react-bootstra
 import api from '../../../../api/index'
 import { dicountFormData } from '../../../../constants/defaultData'
 
-<<<<<<< HEAD
 const DiscountModals = ({ lgShow, handleModalClose, getDiscounts, editAbleObject }) => {
     console.log('editAbleObject=>', editAbleObject);
     const [isEdit, setIsEdit] = useState(editAbleObject ? true : false)
     const [formData, setFormData] = useState(isEdit ? editAbleObject : dicountFormData)
-=======
-const DiscountModals = ({ editAbleObject, lgShow, handleModalClose, getDiscounts }) => {
-    const [isEdit, setIsEdit] = useState(editAbleObject ? true : false)
-    const [formData, setFormData] = useState( isEdit ?  editAbleObject : dicountFormData);
->>>>>>> dev2
     const [validated, setValidated] = useState(false);
     const [isEncourageDiscount, setIsEncourageDiscount] = useState()
     const [itemReached, setItemReached] = useState()
@@ -184,13 +178,8 @@ const DiscountModals = ({ editAbleObject, lgShow, handleModalClose, getDiscounts
                         </Row>
                         <Row>
                             <Col >
-<<<<<<< HEAD
                                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                                     <Form.Check requiredv className="text-sm" defaultChecked={editAbleObject?.remove_previous_discounts} name='remove_previous_discounts' value={applyAfter} onChange={(e) => { checkBoxData(e) }} type="checkbox" label="Remove any discounts applied before this" />
-=======
-                                <Form.Group className="mb-3" controlId="remove_previous_discounts">
-                                    <Form.Check requiredv className="text-sm" defaultValue={editAbleObject?.remove_previous_discounts} name='remove_previous_discounts' value={applyAfter} onChange={(e) => { checkBoxData(e) }} type="checkbox" label="Remove any discounts applied before this" />
->>>>>>> dev2
                                     <p className="text-secondary text-sm">If unchecked, this discount will be added to any other discounts that are applied earlier in the order.</p>
                                 </Form.Group>
                             </Col>
